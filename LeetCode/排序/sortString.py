7d70ab25ba149a5914888cd8c808ee4658e71919
qigaofeng <845214944@qq.com>
2020-03-08 16:27:34 +0800

    上升下降字符串

---
+++ LeetCode/排序/sortString.py
@@ -0,0 +1,24 @@
+def sortString(s):
+    """
+    :type s: str
+    :rtype: str
+    """
+    if not s: return ''
+
+    s = list(s)
+    res = []
+    while s:
+        c_list = list(set(s))
+        c_list.sort(key=lambda c: ord(c))
+        for i in c_list:
+            res.append(i)
+            s.remove(i)
+
+        c_list = list(set(s))
+        c_list.sort(key=lambda c: ord(c), reverse=True)
+        for i in c_list:
+            res.append(i)
+            s.remove(i)
+    return ''.join(res)  # 把列表转换为字符串形式输出
+if __name__ == "__main__":
+    print(sortString("aaaaabbbbbcccc"))
