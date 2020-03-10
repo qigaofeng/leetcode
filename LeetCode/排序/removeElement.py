adabb402285739507ae7cba3614f165c288ac04e
qigaofeng <845214944@qq.com>
2020-03-10 15:36:50 +0800

    移除元素

--- LeetCode/排序/removeElement.py
+++ LeetCode/排序/removeElement.py
@@ -4,6 +4,7 @@ def removeElement(nums, val):
     :type val: int
     :rtype: int
     """
+    # 使用了额外的数组空间
     numss = list(nums)
     for i in nums:
         #print(i)
@@ -13,6 +14,13 @@ def removeElement(nums, val):
     #print(numss)
     #res = len(numss)
     return len(numss)
+
+def removeElement(self, nums: List[int], val: int) -> int:
+    while val in nums:
+        nums.remove(val)
+    return len(nums)
+
+
 
 if __name__ == "__main__":
     print(removeElement([3,2,2,3],3))
