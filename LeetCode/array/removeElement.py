688646f02eecaa314f45331bdcecaa961d49b32b
qigaofeng <845214944@qq.com>
2020-03-11 22:01:46 +0800

    移除元素

---
+++ LeetCode/array/removeElement.py
@@ -0,0 +1,27 @@
+#
+def removeElement(nums, val):
+    """
+    :type nums: List[int]
+    :type val: int
+    :rtype: int
+    """
+    # 使用了额外的数组空间
+    numss = list(nums)
+    for i in nums:
+        # print(i)
+        if i == val:
+            numss.remove(i)
+            # print(numss)
+    # print(numss)
+    # res = len(numss)
+    return len(numss)
+
+
+def removeElement(nums, val):
+    while val in nums:
+        nums.remove(val)
+    return len(nums)
+
+
+if __name__ == "__main__":
+    print(removeElement([3, 2, 2, 3], 3))
