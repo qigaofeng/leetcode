f30bb8c767b71f0ef9f96dc995075df9a5b5f6cb
qigaofeng <845214944@qq.com>
2020-03-15 16:11:22 +0800

    确实的第一个正数

---
+++ LeetCode/array/firstMissingPositive.py
@@ -0,0 +1,10 @@
+def firstMissingPositive(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    i = 1
+    while True:
+        if i not in nums:
+            return i
+        i += 1
