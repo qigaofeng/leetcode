f880b0854cbecf96a2465c4b59f3ae0f23b3827b
qigaofeng <845214944@qq.com>
2020-03-31 14:30:35 +0800

    排序数组

---
+++ LeetCode/每日一题/sortArray.py
@@ -0,0 +1,9 @@
+def sortArray(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: List[int]
+    """
+    return sorted(nums)
+
+if __name__ == "__main__":
+    print(sortArray([1,2,35,1,2]))
