f63f4ebd01c8faada3e23b8001fa2247f941a806
qigaofeng <845214944@qq.com>
2020-04-27 17:17:59 +0800

    二维数组中的查找

--- LeetCode/双指针/findNumberIn2DArray.py
+++ LeetCode/双指针/findNumberIn2DArray.py
@@ -25,5 +25,8 @@ def findNumberIn2DArray( matrix: List[List[int]], target: int) -> bool:
     #         return True
     # return False
 
+
+
+
 if __name__ == "__main__":
-    print(findNumberIn2DArray([[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]],20))
+    print(findNumberIn2DArray([[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]],5))
